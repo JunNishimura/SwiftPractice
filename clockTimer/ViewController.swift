1fa27b64b209b0a99edb864c2124fb3db104e150
JunNishimura <n.junjun0303@gmail.com>
2018-04-24 09:10:57 +0900

    Initial Commit

---
+++ clockTimer/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  clockTimer
+//
+//  Created by 西村純 on 2018/04/24.
+//  Copyright © 2018年 西村純. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // by pressing Restart button, bring it back to this top page
+    @IBAction func returnTop(segue: UIStoryboardSegue) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
