827b2010aea1e599db8b53fa21de631a5da912a7
JunNishimura <n.junjun0303@gmail.com>
2018-05-01 10:37:10 +0900

    Initial Commit

---
+++ SelfIntroduction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfIntroduction
+//
+//  Created by 西村純 on 2018/05/01.
+//  Copyright © 2018年 西村純. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
